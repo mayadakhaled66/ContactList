55508c8467ce6d9b58108bf1eadab117c112b932
Mohamedj23 <mohamedj239@gmail.com>
2019-03-18 21:54:43 +0200

    add new User Method Done

--- lab8/script/page1.js
+++ lab8/script/page1.js
@@ -1,24 +1,17 @@
 function getContact ()
-{
-	
-	/*	var personDataArray = {
-								personName:currentDate,
-								personPhone: textNote,
-								personGender: colorNote ,
-								personEmail:index ,
-								personImage:
-							} ;*/
-						
-	for (var counter = 0 ; counter < localStorage.length ;counter++)
+{		
+	var retreivedArray = JSON.parse(localStorage.getItem('contacts'));		
+	for (var counter = 0 ; counter < retreivedArray.length ;counter++)
 	{
-		var retreivedArray = JSON.parse(localStorage.getItem(localStorage.key(counter)));
 		
-		var pName  = retreivedArray.personName;
-		var pImage = retreivedArray.personImage;
-		var pPhone = retreivedArray.personPhone;
-		var pGender = retreivedArray.personGender;
+		console.log(retreivedArray[counter].name);
+		var pName  = retreivedArray[counter].name;
+		var pPhone = retreivedArray[counter].phone;
+		var pGender = retreivedArray[counter].gender;
+		
 		if (pGender=="female")
 		{
+			
 			$("ul").append('<li><a href="#secondPage"><img src="images/user-female.png" width="50" height="50">'+pName+'<a href=tel:'+pPhone+' data-role="button" class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-phone"></a></a></li>');
 		}
 		else 
@@ -29,3 +22,5 @@ function getContact ()
 		
 	}
 }
+
+window.onload = getContact;
