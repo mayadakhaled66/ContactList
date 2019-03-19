f3e57d7731747d984e500d52cde14dc711625d44
Mohamedj23 <mohamedj239@gmail.com>
2019-03-19 09:21:51 +0200

    Merging Two scripts into one

--- lab8/script/firstPage.js
+++ lab8/script/firstPage.js
@@ -105,9 +105,30 @@ function stopDeleting()
 
 
 
-$(document).on('pagebeforeshow','#thirdPage' , function(e,data){
-	console.log(data.prevPage.find('#userName').text())
-})
+/* Mohamed */
+
+function addNewUser(){
+    var contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    console.log(contacts.length)
+    var userName = $("#name").val();
+    var phoneNum = $("#phone").val();
+    var email = $("#email").val();
+    var gender = $("#gender").val();
+
+   // var person = new Person(userName,phoneNum,email,gender);
+
+    var person = {
+        'name' : userName,
+        'phone' : phoneNum,
+        'email' : email,
+        'gender' : gender
+    };
+
+    contacts.push(person);
+    localStorage.setItem('contacts',JSON.stringify(contacts));
+}
+
+$("#submit").click(addNewUser);
 
 
 
